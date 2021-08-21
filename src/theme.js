c57c429e8e57dad8703fa795c8824dff57998b61
Jada Lilleboe <lilleboejada@gmail.com>
2021-08-21 13:54:40 -0500

    add custom text component

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+const theme = {
+  colors: {
+    primary: '#7851a9',
+    textPrimary: 'black',
+    textSecondary: '#565656'
+  },
+  fontSizes: {
+    body: 14,
+    subheading: 16,
+    heading: 22
+  },
+  fontWeights: {
+    normal: '400',
+    bold: '700',
+  },
+}
+
+export default theme;
