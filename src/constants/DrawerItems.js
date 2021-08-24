0d57a3fd5ff60bb439bb836429336dad7202c17b
Jada Lilleboe <lilleboejada@gmail.com>
2021-08-24 18:28:35 -0500

    maybe making a side menu?

---
+++ src/constants/DrawerItems.js
@@ -0,0 +1,22 @@
+export default [
+  {
+      name:'APOD',
+      iconType:'Material',
+      iconName:'face-profile'
+  },
+  {
+      name:'Objects Near Earth',
+      iconType:'Feather',
+      iconName:'settings'
+  },
+  {
+      name:'Photos From Mars',
+      iconType:'Material',
+      iconName:'bookmark-check-outline'
+  },
+  {
+      name:'Photos From Earth',
+      iconType:'FontAwesome5',
+      iconName:'user-friends'
+  }
+]
