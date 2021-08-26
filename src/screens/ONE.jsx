a1515b0d11e3ae9dafb5d164736f015de0513514
Jada Lilleboe <lilleboejada@gmail.com>
2021-08-26 09:24:54 -0500

    adding near eath objects component

--- src/screens/ONE.jsx
+++ src/screens/ONE.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { ScrollView, SafeAreaView } from 'react-native';
+import Text from '../components/Text';
+import nasaService from '../services/nasa';
 
 const ONE = () => {
+  const [ neows, setNeows ] = useState([]);
+  const fetchONES = async () => {
+    const response = await nasaService.getONES()
+    const json = await response.json();
+    setNeows(json);
+  };
+  useEffect(() => {
+    fetchONES();
+  });
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={{fontSize:16,fontWeight:'700'}}>ONE</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <Text fontSize='heading' padding='paddingAround' align='center'>Asteroids and Objects Near Earth</Text>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
--- src/services/nasa.js
+++ src/services/nasa.js
@@ -1,6 +1,15 @@
+const baseUrl = 'https://stark-cove-38179.herokuapp.com/api/nasa'
+
 const getAPOD = async () => {
-  const response = await fetch('https://stark-cove-38179.herokuapp.com/api/nasa/apod');
+  const response = await fetch(`${baseUrl}/apod`);
   return response
 }
 
-export default { getAPOD }
+const getONES = async () => {
+  console.log('hi')
+  const response = await fetch(`${baseUrl}/neows`)
+  console.log(response)
+  return response
+}
+
+export default { getAPOD, getONES }
