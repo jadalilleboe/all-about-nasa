bdc73376ee2469a57c1ffe50a82b5c682cd6cb58
Jada Lilleboe <lilleboejada@gmail.com>
2021-08-29 16:30:11 -0500

    finish up the mars photos screen

--- src/screens/MarsPhotos.jsx
+++ src/screens/MarsPhotos.jsx
@@ -1,6 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View, FlatList, StyleSheet, Image, Dimensions } from 'react-native';
+import Text from '../components/Text';
+import Loading from '../components/Loading';
 import nasaService from '../services/nasa';
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 30,
+  },
+});
+
+const ItemSeparator = () => <View style={styles.separator} />;
 
 const MarsPhotos = () => {
   const [ marsPhotos, setMarsPhotos ] = useState([])
@@ -12,11 +22,28 @@ const MarsPhotos = () => {
   useEffect(() => {
     fetchMarsPhotos();
   }, [])
-  console.log(marsPhotos)
+  
+  if (marsPhotos.length === 0) {
+    return <Loading />
+  }
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={{fontSize:16,fontWeight:'700'}}>Mars Photos</Text>
-    </View>
+    <>
+    <Text fontSize='heading' padding='paddingAround' align='center'>Recent Photos From The Curiosity Rover</Text>
+    <Text style={{ paddingTop: 0}} padding='paddingAround' align='center'>Photos from {marsPhotos[0].earth_date}</Text>
+    <FlatList 
+      data={marsPhotos}
+      ItemSeparatorComponent={ItemSeparator}
+      renderItem={({ item }) => {
+        return (
+          <View style={{flex: 1, alignItems: 'center'}}>
+          <Image style={{width: (Dimensions.get('window').width - 20), height: 300}} source={{uri: item.img_src}} />
+          <Text>{item.camera.full_name}</Text>
+          </View>
+        )
+      }}
+    />
+    </>
   );
 }
 
